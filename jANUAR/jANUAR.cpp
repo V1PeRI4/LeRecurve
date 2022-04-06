27f9621c4cdfb629a0639da807448b64652e5d8f
user <user@C1014>
2022-04-06 12:31:07 +0300

    Добавьте файлы проекта.

---
+++ jANUAR/jANUAR.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int func(int n, int syst);
+
+int main() 
+{ 
+	setlocale(LC_ALL, "RUS");
+
+	int n, p;
+
+	cout << "Введите число: ";
+	cin >> n;
+	cout << "Введите систему счисления: ";
+	cin >> p;
+
+	if (p < 10)
+	{
+		func(n, p);
+	}
+	else
+	{
+		cout << "чего? ";
+	}
+
+	if (p == 62)
+	{
+		cout << "шыйсят два?!";
+	}
+	
+
+}
+
+int func(int n, int syst)
+{
+
+	string ans = "";
+	int sgn = 1;
+
+	if (n == 0)
+	{
+		cout << 0;
+		return 0;
+	}
+
+	if (n < 0) {
+		sgn = -1;
+	}
+
+	n = abs(n);
+
+	if (n > 0)
+	{
+		ans += ('0' + n % syst);
+		n /= syst;
+
+		func(n, syst);
+	}
+
+	if (sgn == -1)
+	{
+		ans += " nigative";
+	}
+
+	for (int i = 0; i < ans.size(); i++)
+	{
+		cout << ans[i];
+	}
+
+
+}
